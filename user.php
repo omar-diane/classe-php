6c0bbd9855d13ea3906ff4998b7fc641bb5d721c
omar-diane <omar.diane@laplateforme.io>
2022-02-01 12:50:09 +0100

    Premier commit

---
+++ user.php
@@ -0,0 +1,13 @@
+<?php
+
+class user {
+    //__CONSTRUCT
+    function __construct(){
+
+    }
+    //REGISTER
+    public $login, $password, $email, $firstname, $lastname;
+    function register($login, $password, $email, $firstname, $lastname){
+        
+    }
+}
